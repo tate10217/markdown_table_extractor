9d2ae3eb47a46bd1cb0449f3e95310f536f6fddf
tate10217 <check50045366@gmail.com>
2023-12-25 13:51:42 +0900

    行追加削除・列追加　実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: library_private_types_in_public_api
 
-void main() => runApp(MyApp());
+import 'package:flutter/material.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Flutter Markdown Table Extractor',
       home: MarkdownTableExtractor(),
     );
@@ -13,65 +18,71 @@ class MyApp extends StatelessWidget {
 }
 
 class MarkdownTableExtractor extends StatefulWidget {
+  const MarkdownTableExtractor({super.key});
+
   @override
   _MarkdownTableExtractorState createState() => _MarkdownTableExtractorState();
 }
 
 class _MarkdownTableExtractorState extends State<MarkdownTableExtractor> {
-  List<String> tables = [];
+  List<String> tables = []; // マークダウンのテーブルを保持するリスト
+  List<TablePosition> tablePositions = []; // テーブルの位置を保持するリスト
+  String markdownText = '''
+# テスト用マークダウン0
+1
+これは通常のテキストです。以下にマークダウンのテーブルが続きます。2
+3
+| ヘッダ1 | ヘッダ2 | ヘッダ3 | 4 |
+| ------- | ------- | ------- | --- |
+| データ1 | データ2 | データ3 | 6 |
+| データ4 | データ5 | データ6 | 7 |
+
+これはテーブルの間に挿入されたテキストです。9
+10
+## サブセクション11
+12
+- リストアイテム113
+- リストアイテム214
+15
+> 引用文16
+17
+### align属性付きテーブル18
+19
+| 左揃え | 中央揃え | 右揃え | 20 |
+| :----- | :------: | -----: | --- |
+| 左 | 中央 | 右 | 22 |
+
+これは別のセクションです。 24
+25
+### さらに小さいセクション 26
+27
+1. 番号付きリスト1 28
+2. 番号付きリスト2 29
+30
+    コードブロック:31
+    ```
+    print("Hello World")33
+    ```
+35
+以下、別のテーブルです。36
+37
+| 単一のセル | 38 |
+| ----------- | --- |
+| 単一のデータ | 40 |
+
+これでテスト用のマークダウン文字列は終わりです。42
+''';
 
   @override
   void initState() {
     super.initState();
-    String markdownText = '''
-    # テスト用マークダウン
-
-    これは通常のテキストです。以下にマークダウンのテーブルが続きます。
-
-    | ヘッダ1 | ヘッダ2 | ヘッダ3 |
-    | ------- | ------- | ------- |
-    | データ1 | データ2 | データ3 |
-    | データ4 | データ5 | データ6 |
-
-    これはテーブルの間に挿入されたテキストです。
-
-    ## サブセクション
-
-    - リストアイテム1
-    - リストアイテム2
-
-    > 引用文
-
-    ### align属性付きテーブル
-
-    | 左揃え | 中央揃え | 右揃え |
-    | :----- | :------: | -----: |
-    | 左     | 中央     | 右     |
-
-    これは別のセクションです。
-
-    ### さらに小さいセクション
-
-    1. 番号付きリスト1
-    2. 番号付きリスト2
-
-        コードブロック:
-        ```
-        print("Hello World")
-        ```
-
-    以下、別のテーブルです。
-
-    | 単一のセル |
-    | ----------- |
-    | 単一のデータ |
-
-    これでテスト用のマークダウン文字列は終わりです。
-    ''';
-
     tables = extractMarkdownTables(markdownText, includeHeading: true);
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+    print(markdownText);
+    print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
   }
 
+  /// マークダウンテキストからテーブルを抽出
   List<String> extractMarkdownTables(String markdown,
       {bool includeHeading = true}) {
     List<String> lines = markdown.split('\n'); // Markdownテキストを行に分割
@@ -80,6 +91,8 @@ class _MarkdownTableExtractorState extends State<MarkdownTableExtractor> {
     bool textBetweenHeadingAndTable = false; // 見出しとテーブルの間にテキストがあるかを追跡
     StringBuffer currentTable = StringBuffer(); // 現在処理中のテーブルを構築
     String lastHeading = ''; // 最後に見つかった見出しを保持
+    int startLineIndex = -1; // テーブルの開始位置を追跡
+    int endLineIndex = -1; // テーブルの終了位置を追跡
 
     for (String line in lines) {
       var trimmedLine = line.trim(); // 行の前後の空白を削除
@@ -135,6 +148,24 @@ class _MarkdownTableExtractorState extends State<MarkdownTableExtractor> {
         inTable = false;
         lastHeading = '';
       }
+
+      // テーブルの開始を追跡
+      if (inTable && startLineIndex == -1) {
+        startLineIndex = lines.indexOf(line); // テーブルの開始行を設定
+        print('--------------------------------------------------');
+        print('テーブルの開始行: $startLineIndex');
+        print('$line');
+      }
+      // テーブルの終了を追跡
+      if ((!inTable || line.trim() == '') && startLineIndex != -1) {
+        endLineIndex = lines.indexOf(line) - 1; // テーブルの終了行を設定
+        tablePositions.add(
+            TablePosition(startLine: startLineIndex, endLine: endLineIndex));
+        startLineIndex = -1; // リセット
+        print('テーブルの終了行: $endLineIndex');
+        print('${lines[endLineIndex]}');
+        print('--------------------------------------------------');
+      }
     }
 
     // 最後のテーブルを処理
@@ -145,6 +176,83 @@ class _MarkdownTableExtractorState extends State<MarkdownTableExtractor> {
     return tables;
   }
 
+  /// 編集されたテーブルを元のマークダウンテキストにマージ
+  void mergeEditedTablesIntoMarkdown() {
+    List<String> originalLines = markdownText.split('\n');
+    List<String> mergedLines = [];
+    int currentTableIndex = 0;
+    // tablePositionsをdeepコピー
+    List<TablePosition> newTablePositions = [];
+    for (int i = 0; i < tablePositions.length; i++) {
+      newTablePositions.add(TablePosition(
+          startLine: tablePositions[i].startLine,
+          endLine: tablePositions[i].endLine));
+    }
+
+    for (int i = 0; i < originalLines.length; i++) {
+      List<String> aaa = []; //! debug
+      if (currentTableIndex < tables.length &&
+          i == tablePositions[currentTableIndex].startLine) {
+        List<String> newTableLines = tables[currentTableIndex].split('\n');
+        int offset = 0; // 現在のオフセット（行数の変化）
+
+        // デバッグ出力: 編集前のテーブル位置と内容
+        print('...................................................');
+        print('編集前 - テーブル $currentTableIndex: '
+            '開始行 ${tablePositions[currentTableIndex].startLine}, '
+            '終了行 ${tablePositions[currentTableIndex].endLine}');
+        print('編集前のテーブル内容:');
+        for (int k = tablePositions[currentTableIndex].startLine;
+            k <= tablePositions[currentTableIndex].endLine;
+            k++) {
+          print(originalLines[k]);
+        }
+
+        // 新しいテーブルの内容を追加
+        mergedLines.addAll(newTableLines);
+
+        // オフセット（行数の変化）を計算
+        int originalTableLength = tablePositions[currentTableIndex].endLine -
+            tablePositions[currentTableIndex].startLine +
+            1;
+        int editedTableLength = newTableLines.length;
+        offset = editedTableLength - originalTableLength;
+
+        // デバッグ出力: 編集されたテーブルの内容と行数
+        print('編集されたテーブルの内容（$editedTableLength行）:');
+        newTableLines.forEach((line) => print(line));
+
+        // 元のテーブルの範囲をスキップ
+        i = tablePositions[currentTableIndex].endLine;
+
+        // 次のテーブルの位置を更新
+        for (int j = currentTableIndex + 0; j < tablePositions.length; j++) {
+          if (j > currentTableIndex) newTablePositions[j].startLine += offset;
+          newTablePositions[j].endLine += offset;
+          // デバッグ出力: offset
+          aaa.add('\noffset: $offset  -> テーブル $j: '
+              '開始行 ${newTablePositions[j].startLine}, '
+              '終了行 ${newTablePositions[j].endLine}');
+        }
+
+        // デバッグ出力: 編集後のテーブル位置
+        print('編集後 - テーブル $currentTableIndex: '
+            '開始行 ${tablePositions[currentTableIndex].startLine}, '
+            '終了行 ${tablePositions[currentTableIndex].endLine}');
+        print(aaa);
+        print('...................................................');
+
+        currentTableIndex++;
+      } else if (currentTableIndex >= tables.length ||
+          i < tablePositions[currentTableIndex].startLine) {
+        mergedLines.add(originalLines[i]);
+      }
+    }
+
+    tablePositions = newTablePositions;
+    markdownText = mergedLines.join('\n');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -152,58 +260,72 @@ class _MarkdownTableExtractorState extends State<MarkdownTableExtractor> {
         title: const Text('Markdown Table Extractor and Editor'),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            ListView.builder(
-              shrinkWrap: true, // このリストの高さをそのコンテンツに合わせる
-              physics:
-                  const NeverScrollableScrollPhysics(), // このリストのスクロールを無効にする
-              itemCount: tables.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: EditableTableWidget(
-                      tableMarkdown: tables[index],
-                      readOnly: false,
-                      titleAlignment: TextAlign.left,
-                      headerAlignment: TextAlign.center,
-                      allowColumnEditing: true,
-                      allowRowEditing: true,
-                      onTableChanged: (markdown) {
-                        setState(() {
-                          tables[index] = markdown;
-                        });
-                      },
-                    ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: Column(children: [
+                  ListView.builder(
+                    shrinkWrap: true, // このリストの高さをそのコンテンツに合わせる
+                    physics:
+                        const NeverScrollableScrollPhysics(), // このリストのスクロールを無効にする
+                    itemCount: tables.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: EditableTableWidget(
+                            tableMarkdown: tables[index],
+                            readOnly: false,
+                            titleAlignment: TextAlign.left,
+                            headerAlignment: TextAlign.center,
+                            allowColumnEditing: true,
+                            allowRowEditing: true,
+                            onTableChanged: (markdown) {
+                              setState(() {
+                                tables[index] = markdown;
+                                mergeEditedTablesIntoMarkdown();
+                                print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+                                print(markdownText);
+                                print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+                              });
+                            },
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
-            const SizedBox(height: 24),
-            // markdownテーブル文字列を表示する
-            ListView.builder(
-              shrinkWrap: true, // このリストの高さをそのコンテンツに合わせる
-              physics:
-                  const NeverScrollableScrollPhysics(), // このリストのスクロールを無効にする
-              itemCount: tables.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(tables[index]),
-                  ),
-                );
-              },
-            ),
-          ],
+                ]),
+              ),
+              // markdownを表示する
+              const SizedBox(height: 24),
+              Card(
+                child: Container(
+                  width: 500,
+                  padding: const EdgeInsets.all(8.0),
+                  child: MarkdownBody(data: markdownText),
+                ),
+              ),
+              // markdown文字列を表示する
+              const SizedBox(height: 24),
+              Card(
+                child: Container(
+                  width: 500,
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(markdownText),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-/* -------------------------------------------------------------------------- */
+/* ---------------------------------------------------------------------------------------------------------------------------------------------------- */
 class EditableTableWidget extends StatefulWidget {
   final String tableMarkdown;
   final bool readOnly;
@@ -263,8 +385,16 @@ class _EditableTableWidgetState extends State<EditableTableWidget> {
   }
 
   void _onFieldChanged() {
-    var markdownTable = title != '' ? '$title\n' : '';
+    var markdownTable = '';
     markdownTable += _controllers.map((row) {
+      // 2行目だけテーブルの区切り文字が設定されていなければ、区切り文字を追加
+      if (_controllers.indexOf(row) == 1) {
+        for (int i = 0; i < row.length; i++) {
+          if (row[i].text.trim() == '') {
+            row[i].text = '---';
+          }
+        }
+      }
       return '| ${row.map((cell) => cell.text).join(' | ')} |';
     }).join('\n');
     markdownTable += '\n';
@@ -374,7 +504,17 @@ class _EditableTableWidgetState extends State<EditableTableWidget> {
         cellWidgets.add(
           IconButton.filled(
               tooltip: '行を削除',
-              icon: const Icon(Icons.remove),
+              icon: Stack(children: [
+                const Icon(Icons.table_rows_outlined),
+                Positioned(
+                  right: 0,
+                  bottom: 0,
+                  child: Container(
+                      color: Theme.of(context).colorScheme.primary,
+                      child: const Icon(Icons.remove_circle,
+                          size: 14, weight: 700, grade: 200, opticalSize: 48)),
+                ),
+              ]),
               onPressed: () => _removeRow(i)),
         );
       } else if (widget.allowColumnEditing) {
@@ -392,14 +532,29 @@ class _EditableTableWidgetState extends State<EditableTableWidget> {
             child: Padding(
               padding: const EdgeInsets.only(top: 8),
               child: IconButton.filledTonal(
-                  tooltip: '行を追加',
-                  icon: const Icon(Icons.add),
-                  onPressed: _addRow),
+                tooltip: '行を追加',
+                icon: Stack(children: [
+                  const Icon(Icons.table_rows_outlined),
+                  Positioned(
+                    right: 0,
+                    bottom: 0,
+                    child: Container(
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                        child: const Icon(Icons.add_circle,
+                            size: 14,
+                            weight: 700,
+                            grade: 200,
+                            opticalSize: 48)),
+                  ),
+                ]),
+                onPressed: _addRow,
+              ),
             ),
           ),
         ]),
       );
     }
+
     return Column(children: [
       if (title.trim() != '')
         Row(children: [
@@ -419,7 +574,20 @@ class _EditableTableWidgetState extends State<EditableTableWidget> {
           widget.allowColumnEditing
               ? IconButton.filledTonal(
                   tooltip: '列を追加',
-                  icon: const Icon(Icons.add),
+                  icon: Stack(children: [
+                    const Icon(Icons.view_column_outlined),
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      child: Container(
+                          color: Theme.of(context).colorScheme.primaryContainer,
+                          child: const Icon(Icons.add_circle,
+                              size: 14,
+                              weight: 700,
+                              grade: 200,
+                              opticalSize: 48)),
+                    ),
+                  ]),
                   onPressed: _addColumn)
               : widget.allowRowEditing
                   ? const IconButton(onPressed: null, icon: Icon(null))
@@ -430,4 +598,12 @@ class _EditableTableWidgetState extends State<EditableTableWidget> {
       Column(children: rowWidgets),
     ]);
   }
+}
+
+/* ---------------------------------------------------------------------------------------------------------------------------------------------------- */
+class TablePosition {
+  int startLine;
+  int endLine;
+
+  TablePosition({required this.startLine, required this.endLine});
 }
